2c4fc22e44f41278e7a13d571096eba2aa367718
Greg Kapka <greg@oraclize.it>
2021-09-08 10:45:51 +0100

    feat(truffle-config): <- update that to use new configurables

--- truffle-config.js
+++ truffle-config.js
@@ -1,38 +1,80 @@
-require('dotenv').config()
+const {
+  ENDPOINT,
+  GAS_PRICE,
+  PRIVATE_KEY,
+  ETHERSCAN_API_KEY,
+} = require('./config')
 const WalletProvider = require('@truffle/hdwallet-provider')
 
-const getEnvironmentVariable = _envVar =>
-  process.env[_envVar]
-    ? process.env[_envVar]
-    : (
-      console.error(
-        '✘ Cannot migrate!',
-        '✘ Please provide an infura api key as and an',
-        '✘ account private key as environment variables:',
-        '✘ PRIVATE_KEY',
-        '✘ INFURA_KEY'
-      ),
-      process.exit(1)
-    )
+const GAS_LIMIT = 6e6
+const getProvider = _ => new WalletProvider(PRIVATE_KEY, ENDPOINT)
 
 module.exports = {
   networks: {
     development: {
-      host: '127.0.0.1',
       port: 8545,
       network_id: '*',
-      websockets: true
+      websockets: true,
+      host: '127.0.0.1',
+    },
+    ethMainnet: {
+      network_id: 1,
+      confirmations: 1,
+      gas: GAS_LIMIT,
+      gasPrice: GAS_PRICE,
+      provider: _ => getProvider(),
     },
     rinkeby: {
-      provider: () =>
-        new WalletProvider(
-          getEnvironmentVariable('PRIVATE_KEY'),
-          'http://localhost:8545'
-        ),
       network_id: 4,
-      gas: 6e6,
-      gasPrice: 5e9,
-      websockets: true
+      gas: GAS_LIMIT,
+      confirmations: 1,
+      websockets: true,
+      gasPrice: GAS_PRICE,
+      provider: _ => getProvider(),
+    },
+    ropsten: {
+      network_id: 3,
+      gas: GAS_LIMIT,
+      confirmations: 1,
+      websockets: true,
+      gasPrice: GAS_PRICE,
+      provider: _ => getProvider(),
+    },
+    bscMainnet: {
+      network_id: 56,
+      gas: GAS_LIMIT,
+      confirmations: 1,
+      skipDryRun: true,
+      timeoutBlocks: 500,
+      gasPrice: GAS_PRICE,
+      provider: _ => getProvider(),
+    },
+    bscTestnet: {
+      gas: GAS_LIMIT,
+      network_id: 97,
+      confirmations: 1,
+      skipDryRun: true,
+      timeoutBlocks: 500,
+      gasPrice: GAS_PRICE,
+      provider: _ => getProvider(),
+    },
+    xDai: {
+      gas: GAS_LIMIT,
+      network_id: 100,
+      confirmations: 1,
+      skipDryRun: true,
+      timeoutBlocks: 10,
+      gasPrice: GAS_PRICE,
+      provider: _ => getProvider(),
+    },
+    polygonMaticMainnet: {
+      gas: GAS_LIMIT,
+      network_id: 137,
+      confirmations: 1,
+      timeoutBlocks: 10,
+      gasPrice: GAS_PRICE,
+      networkCheckTimeout: 10000,
+      provider: _ => getProvider(),
     },
   },
   compilers: {
@@ -48,5 +90,11 @@ module.exports = {
   },
   mocha: {
     enableTimeouts: false
-  }
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: ETHERSCAN_API_KEY,
+  },
 }
