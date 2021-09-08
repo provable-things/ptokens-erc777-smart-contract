202add62fc91b115a4877e4d7b8f036bbb453905
Greg Kapka <greg@oraclize.it>
2021-09-08 10:45:36 +0100

    feat(migrations): <- update those to use new configurables

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,8 @@
+const {
+  TOKEN_NAME,
+  TOKEN_SYMBOL,
+  ADMIN_ADDRESS,
+} = require('../config')
 const { deployProxy } = require('@openzeppelin/truffle-upgrades')
 const { singletons } = require('@openzeppelin/test-helpers')
 require('@openzeppelin/test-helpers/configure')({
@@ -7,12 +12,11 @@ require('@openzeppelin/test-helpers/configure')({
 const PToken = artifacts.require('PToken')
 
 module.exports = async (deployer, network, accounts) => {
-  if (network.includes('develop'))
-    await singletons.ERC1820Registry(accounts[0])
+  if (network.includes('develop')) await singletons.ERC1820Registry(accounts[0])
   const instance = await deployProxy(
     PToken,
-    [ 'pToken', 'pTOK', accounts[0] ],
+    [ TOKEN_NAME, TOKEN_SYMBOL, ADMIN_ADDRESS ],
     { deployer, initializer: 'initialize' }
   )
-  console.info('\nDeployed address:', instance.address, '\n')
+  console.info(`\nDeployed @ address ${instance.address}\n`)
 }
