4fd4fe6ba16f77a9011ae400956e85ab10561179
Greg Kapka <greg@oraclize.it>
2021-10-28 21:51:22 +0100

    feat(main): <- adds pToken deployment pathway

--- ptoken-deployer.js
+++ ptoken-deployer.js
@@ -3,11 +3,12 @@
 require('dotenv').config()
 const { docopt } = require('docopt')
 const { version } = require('./package.json')
-
+const { deployPTokenContract } = require('./lib/deploy-ptoken.js')
 
 const HELP_ARG = '--help'
 const VERSION_ARG = '--version'
 const TOOL_NAME = 'erc777-deployer.js'
+const DEPLOY_PTOKEN_CMD = 'deployPToken'
 
 const USAGE_INFO = `
 ❍ pTokens ERC777 Deployer ❍
@@ -22,17 +23,21 @@ const USAGE_INFO = `
 ❍ Usage:
   ${TOOL_NAME} ${HELP_ARG}
   ${TOOL_NAME} ${VERSION_ARG}
+  ${TOOL_NAME} ${DEPLOY_PTOKEN_CMD}
 
 ❍ Commands:
 
+  ${DEPLOY_PTOKEN_CMD}          ❍ Deploy the pToken logic contract.
 
 ❍ Options:
     ${HELP_ARG}              ❍ Show this message.
     ${VERSION_ARG}           ❍ Show version information.
+    ${DEPLOY_PTOKEN_CMD}        ❍ Deploy the pToken logic contract.
 `
 
 const main = _ => {
   const CLI_ARGS = docopt(USAGE_INFO, { version })
+  if (CLI_ARGS[DEPLOY_PTOKEN_CMD]) return deployPTokenContract()
 }
 
-main()
+main().catch(_err => console.error(_err.message))
