b0f1dd18673f65075f882e0da9a97017af146379
Greg Kapka <greg@oraclize.it>
2021-11-01 14:45:30 +0000

    feat(main): <- adds fxn to verify the deployed contract

--- ptoken-deployer.js
+++ ptoken-deployer.js
@@ -4,11 +4,15 @@ require('dotenv').config()
 const { docopt } = require('docopt')
 const { version } = require('./package.json')
 const { deployPTokenContract } = require('./lib/deploy-ptoken.js')
+const { verifyPTokenContract } = require('./lib/verify-ptoken.js')
 
 const HELP_ARG = '--help'
 const VERSION_ARG = '--version'
-const TOOL_NAME = 'erc777-deployer.js'
+const NETWORK_ARG = '<network>'
+const TOOL_NAME = 'ptoken-deployer.js'
 const DEPLOY_PTOKEN_CMD = 'deployPToken'
+const VERIFY_PTOKEN_CMD = 'verifyPToken'
+const DEPLOYED_ADDRESS_ARG = '<deployedAddress>'
 
 const USAGE_INFO = `
 ❍ pTokens ERC777 Deployer ❍
@@ -24,20 +28,26 @@ const USAGE_INFO = `
   ${TOOL_NAME} ${HELP_ARG}
   ${TOOL_NAME} ${VERSION_ARG}
   ${TOOL_NAME} ${DEPLOY_PTOKEN_CMD}
+  ${TOOL_NAME} ${VERIFY_PTOKEN_CMD} ${DEPLOYED_ADDRESS_ARG} ${NETWORK_ARG}
 
 ❍ Commands:
 
   ${DEPLOY_PTOKEN_CMD}          ❍ Deploy the pToken logic contract.
+  ${VERIFY_PTOKEN_CMD}          ❍ Verify a deployed pToken logic contract.
 
 ❍ Options:
     ${HELP_ARG}              ❍ Show this message.
     ${VERSION_ARG}           ❍ Show version information.
-    ${DEPLOY_PTOKEN_CMD}        ❍ Deploy the pToken logic contract.
+    ${DEPLOYED_ADDRESS_ARG}   ❍ The address the pToken is deployed at.
+    ${NETWORK_ARG}           ❍ Network the pToken is deployed on. It must exist in the 'hardhat.config.json'.
 `
 
 const main = _ => {
   const CLI_ARGS = docopt(USAGE_INFO, { version })
-  if (CLI_ARGS[DEPLOY_PTOKEN_CMD]) return deployPTokenContract()
+  if (CLI_ARGS[DEPLOY_PTOKEN_CMD])
+    return deployPTokenContract()
+  else if (CLI_ARGS[VERIFY_PTOKEN_CMD])
+    return verifyPTokenContract(CLI_ARGS[DEPLOYED_ADDRESS_ARG], CLI_ARGS[NETWORK_ARG])
 }
 
 main().catch(_err => console.error('✘', _err.message))
