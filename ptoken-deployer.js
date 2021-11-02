1a21e69513f4416be5308b58dbab7f83e7298207
Greg Kapka <greg@oraclize.it>
2021-11-02 10:23:53 +0000

    feat(main): <- adds CLI fxn to show suggested fees

--- ptoken-deployer.js
+++ ptoken-deployer.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const { docopt } = require('docopt')
 const { version } = require('./package.json')
+const { showSuggestedFees } = require('./lib/show-suggested-fees')
 const { deployPTokenContract } = require('./lib/deploy-ptoken.js')
 const { verifyPTokenContract } = require('./lib/verify-ptoken.js')
 const { getEncodedInitArgs } = require('./lib/get-encoded-init-args.js')
@@ -10,14 +11,15 @@ const { getEncodedInitArgs } = require('./lib/get-encoded-init-args.js')
 const HELP_ARG = '--help'
 const VERSION_ARG = '--version'
 const NETWORK_ARG = '<network>'
+const TOKEN_NAME_ARG = '<tokenName>'
 const TOOL_NAME = 'ptoken-deployer.js'
+const TOKEN_SYMBOL_ARG = '<tokenSymbol>'
 const DEPLOY_PTOKEN_CMD = 'deployPToken'
 const VERIFY_PTOKEN_CMD = 'verifyPToken'
+const SHOW_SUGGESTED_FEES = 'showSuggestedFees'
 const DEPLOYED_ADDRESS_ARG = '<deployedAddress>'
-const GET_ENCODED_INIT_ARGS_CMD = 'getEncodedInitArgs'
-const TOKEN_NAME_ARG = '<tokenName>'
-const TOKEN_SYMBOL_ARG = '<tokenSymbol>'
 const TOKEN_ADMIN_ADDRESS_ARG = '<adminAddress>'
+const GET_ENCODED_INIT_ARGS_CMD = 'getEncodedInitArgs'
 
 const USAGE_INFO = `
 ❍ pTokens ERC777 Deployer ❍
@@ -33,22 +35,24 @@ const USAGE_INFO = `
   ${TOOL_NAME} ${HELP_ARG}
   ${TOOL_NAME} ${VERSION_ARG}
   ${TOOL_NAME} ${DEPLOY_PTOKEN_CMD}
+  ${TOOL_NAME} ${SHOW_SUGGESTED_FEES}
   ${TOOL_NAME} ${VERIFY_PTOKEN_CMD} ${DEPLOYED_ADDRESS_ARG} ${NETWORK_ARG}
   ${TOOL_NAME} ${GET_ENCODED_INIT_ARGS_CMD} ${TOKEN_NAME_ARG} ${TOKEN_SYMBOL_ARG} ${TOKEN_ADMIN_ADDRESS_ARG}
 
 ❍ Commands:
 
+  ${SHOW_SUGGESTED_FEES}     ❍ Show 'ethers.js' suggested fees.
   ${DEPLOY_PTOKEN_CMD}          ❍ Deploy the pToken logic contract.
   ${VERIFY_PTOKEN_CMD}          ❍ Verify a deployed pToken logic contract.
   ${GET_ENCODED_INIT_ARGS_CMD}    ❍ Calculate the initializer function arguments in ABI encoded format.
 
 ❍ Options:
     ${HELP_ARG}              ❍ Show this message.
-    ${VERSION_ARG}           ❍ Show version information.
+    ${VERSION_ARG}           ❍ Show tool version.
     ${TOKEN_NAME_ARG}         ❍ The name of the pToken.
     ${TOKEN_SYMBOL_ARG}       ❍ The symbol of the pToken.
-    ${DEPLOYED_ADDRESS_ARG}   ❍ The address the pToken is deployed at.
-    ${TOKEN_ADMIN_ADDRESS_ARG}      ❍ The ETH address that administrates the pToken.
+    ${DEPLOYED_ADDRESS_ARG}   ❍ The ETH address of the deployed pToken.
+    ${TOKEN_ADMIN_ADDRESS_ARG}      ❍ The ETH address which administrates the pToken.
     ${NETWORK_ARG}           ❍ Network the pToken is deployed on. It must exist in the 'hardhat.config.json'.
 `
 
@@ -59,8 +63,14 @@ const main = _ => {
   } else if (CLI_ARGS[VERIFY_PTOKEN_CMD]) {
     return verifyPTokenContract(CLI_ARGS[DEPLOYED_ADDRESS_ARG], CLI_ARGS[NETWORK_ARG])
   } else if (CLI_ARGS[GET_ENCODED_INIT_ARGS_CMD]) {
-    return getEncodedInitArgs(CLI_ARGS[TOKEN_NAME_ARG], CLI_ARGS[TOKEN_SYMBOL_ARG], CLI_ARGS[TOKEN_ADMIN_ADDRESS_ARG])
+    return getEncodedInitArgs(
+      CLI_ARGS[TOKEN_NAME_ARG],
+      CLI_ARGS[TOKEN_SYMBOL_ARG],
+      CLI_ARGS[TOKEN_ADMIN_ADDRESS_ARG]
+    )
       .then(console.info)
+  } else if(CLI_ARGS[SHOW_SUGGESTED_FEES_CMD]) {
+    return showSuggestedFees().then(console.table)
   }
 }
 
