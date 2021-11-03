fae5f8b0be63da1670231eb7dfc78239fb882fb8
Greg Kapka <greg@oraclize.it>
2021-11-03 19:32:58 +0000

    feat(main): <- adds new CLI function to make peg outs

--- ptoken-deployer.js
+++ ptoken-deployer.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config()
 const { docopt } = require('docopt')
+const { pegOut } = require('./lib/peg-out')
 const { version } = require('./package.json')
 const { showBalanceOf } = require('./lib/get-balance-of')
 const { flattenContract } = require('./lib/flatten-contract')
@@ -13,6 +14,8 @@ const { getEncodedInitArgs } = require('./lib/get-encoded-init-args')
 const { showExistingPTokenContractAddresses } = require('./lib/show-existing-logic-contract-addresses')
 
 const HELP_ARG = '--help'
+const PEG_OUT_CMD = 'pegOut'
+const AMOUNT_ARG = '<amount>'
 const VERSION_ARG = '--version'
 const NETWORK_ARG = '<network>'
 const TOKEN_NAME_ARG = '<tokenName>'
@@ -22,12 +25,15 @@ const TOOL_NAME = 'ptoken-deployer.js'
 const TOKEN_SYMBOL_ARG = '<tokenSymbol>'
 const DEPLOY_PTOKEN_CMD = 'deployPToken'
 const VERIFY_PTOKEN_CMD = 'verifyPToken'
+const USER_DATA_OPTIONAL_ARG = '--userData'
+const RECIPIENT_ADDRESS_ARG = '<recipient>'
 const FLATTEN_CONTRACT_CMD = 'flattenContract'
 const GRANT_MINTER_ROLE_CMD = 'grantMinterRole'
 const DEPLOYED_ADDRESS_ARG = '<deployedAddress>'
 const TOKEN_ADMIN_ADDRESS_ARG = '<adminAddress>'
 const SHOW_SUGGESTED_FEES_CMD = 'showSuggestedFees'
 const GET_ENCODED_INIT_ARGS_CMD = 'getEncodedInitArgs'
+const USER_DATA_ARG = `${USER_DATA_OPTIONAL_ARG}=<hex>`
 const SHOW_EXISTING_CONTRACTS_CMD = 'showExistingContracts'
 
 const USAGE_INFO = `
@@ -51,6 +57,7 @@ const USAGE_INFO = `
   ${TOOL_NAME} ${GET_BALANCE_CMD} ${DEPLOYED_ADDRESS_ARG} ${ETH_ADDRESS_ARG}
   ${TOOL_NAME} ${GRANT_MINTER_ROLE_CMD} ${DEPLOYED_ADDRESS_ARG} ${ETH_ADDRESS_ARG}
   ${TOOL_NAME} ${GET_ENCODED_INIT_ARGS_CMD} ${TOKEN_NAME_ARG} ${TOKEN_SYMBOL_ARG} ${TOKEN_ADMIN_ADDRESS_ARG}
+  ${TOOL_NAME} ${PEG_OUT_CMD} ${DEPLOYED_ADDRESS_ARG} ${AMOUNT_ARG} ${RECIPIENT_ADDRESS_ARG} [${USER_DATA_ARG}]
 
 ❍ Commands:
 
@@ -59,6 +66,7 @@ const USAGE_INFO = `
   ${VERIFY_PTOKEN_CMD}          ❍ Verify a deployed pToken logic contract.
   ${GET_BALANCE_CMD}          ❍ Get balance of ${ETH_ADDRESS_ARG} of pToken at ${DEPLOYED_ADDRESS_ARG}.
   ${GET_ENCODED_INIT_ARGS_CMD}    ❍ Calculate the initializer function arguments in ABI encoded format.
+  ${PEG_OUT_CMD}                ❍ Redeem ${AMOUNT_ARG} at ${DEPLOYED_ADDRESS_ARG} with optional ${USER_DATA_ARG}.
   ${FLATTEN_CONTRACT_CMD}       ❍ Flatten the pToken contract in case manual verification is required.
   ${GRANT_MINTER_ROLE_CMD}       ❍ Grant a minter role to ${ETH_ADDRESS_ARG} for pToken at ${DEPLOYED_ADDRESS_ARG}.
   ${SHOW_EXISTING_CONTRACTS_CMD} ❍ Show list of existing pToken logic contract addresses on various blockchains.
@@ -70,7 +78,10 @@ const USAGE_INFO = `
   ${TOKEN_NAME_ARG}           ❍ The name of the pToken.
   ${TOKEN_SYMBOL_ARG}         ❍ The symbol of the pToken.
   ${DEPLOYED_ADDRESS_ARG}     ❍ The ETH address of the deployed pToken.
+  ${RECIPIENT_ADDRESS_ARG}           ❍ The recipient of the pegged out pTokens.
+  ${USER_DATA_ARG}      ❍ Optional user data in hex format [default: 0x].
   ${TOKEN_ADMIN_ADDRESS_ARG}        ❍ The ETH address which administrates the pToken.
+  ${AMOUNT_ARG}              ❍ An amount in the most granular form of the token.
   ${NETWORK_ARG}             ❍ Network the pToken is deployed on. It must exist in the 'hardhat.config.json'.
 `
 
@@ -97,6 +108,13 @@ const main = _ => {
     return grantMinterRole(CLI_ARGS[DEPLOYED_ADDRESS_ARG], CLI_ARGS[ETH_ADDRESS_ARG])
   } else if (CLI_ARGS[GET_BALANCE_CMD]) {
     return showBalanceOf(CLI_ARGS[DEPLOYED_ADDRESS_ARG], CLI_ARGS[ETH_ADDRESS_ARG])
+  } else if (CLI_ARGS[PEG_OUT_CMD]) {
+    return pegOut(
+      CLI_ARGS[DEPLOYED_ADDRESS_ARG],
+      CLI_ARGS[AMOUNT_ARG],
+      CLI_ARGS[RECIPIENT_ADDRESS_ARG],
+      CLI_ARGS[USER_DATA_OPTIONAL_ARG],
+    )
   }
 }
 
