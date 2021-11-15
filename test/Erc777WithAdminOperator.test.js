10e9c2ba13274c786bcd4333500ebd5dc462ccb6
Greg Kapka <greg@oraclize.it>
2021-11-15 12:37:51 +0000

    feat(admin-operator-tests): <- switch those to hardhat

--- test/Erc777WithAdminOperator.test.js
+++ test/Erc777WithAdminOperator.test.js
@@ -1,43 +1,59 @@
 const {
-  expectEvent,
-  expectRevert,
-} = require('@openzeppelin/test-helpers')
-const { expect } = require('chai')
-const { deployProxy } = require('@openzeppelin/truffle-upgrades')
-const pToken = artifacts.require('PToken.sol')
+  EMPTY_DATA,
+  PTOKEN_CONTRACT_PATH,
+} = require('./test-constants')
+const assert = require('assert')
+const { BigNumber } = require('ethers')
+const { assertTransferEvent } = require('./test-utils')
 
-contract('pToken/ERC777WithAdminOperator', ([ OWNER, NON_OWNER, ADMIN_OPERATOR ]) => {
-  let pTokenContract
+describe('Admin Operator Tests...', () => {
+  let owner, nonOwner, adminOperator, pTokenContract
 
   beforeEach(async () => {
-    pTokenContract = await deployProxy(pToken, [ 'pToken', 'pTOK', OWNER ])
-    await pTokenContract.grantMinterRole(OWNER, { from: OWNER, gas: 300000 })
-    await pTokenContract.mint(OWNER, 100000, { from: OWNER })
-    await pTokenContract.setAdminOperator(ADMIN_OPERATOR, { from: OWNER })
+    [ owner, nonOwner, adminOperator ] = await ethers.getSigners()
+    const contractFactory = await ethers.getContractFactory(PTOKEN_CONTRACT_PATH)
+    pTokenContract = await upgrades.deployProxy(contractFactory, ['Test', 'TST', owner.address])
+    await pTokenContract.grantMinterRole(owner.address)
+    await pTokenContract['mint(address,uint256)'](owner.address, 100000)
+    await pTokenContract.setAdminOperator(adminOperator.address)
   })
 
-  it('OWNER cannot change the admin operator', async () => {
-    await expectRevert(
-      pTokenContract.setAdminOperator(NON_OWNER, { from: OWNER }),
-      'Only the actual admin operator can change the address'
-    )
+  it('Non-owner cannot change the admin operator address', async () => {
+    const nonOwnedPTokenContract = pTokenContract.connect(nonOwner)
+    try {
+      await nonOwnedPTokenContract.setAdminOperator(nonOwner.address)
+      assert.fail('Should not have succeeded!')
+    } catch (_err) {
+      const expectedErr = 'Only the actual admin operator can change the address'
+      assert(_err.message.includes(expectedErr))
+    }
   })
 
-  it('Admin operator can change the admin operator address', async () => {
-    await pTokenContract.setAdminOperator(NON_OWNER, { from: ADMIN_OPERATOR })
-    expect(await pTokenContract.adminOperator()).to.equal(NON_OWNER)
+  it('Admin operator CAN change the admin operator address', async () => {
+    const newAddress = nonOwner.address
+    const contract = pTokenContract.connect(adminOperator)
+    await contract.setAdminOperator(newAddress)
+    const adminOperatorAddressInContract = await contract.adminOperator()
+    assert.strictEqual(adminOperatorAddressInContract, newAddress)
   })
 
-  it('adminTransfer() should fail if the caller is not the admin operator', async () => {
-    await expectRevert(
-      pTokenContract.adminTransfer(OWNER, NON_OWNER, 1, '0x', '0x', { from: OWNER }),
-      'caller is not the admin operator'
-    )
+  it('`adminTransfer()` should fail if the caller is not the admin operator', async () => {
+    try {
+      await pTokenContract.adminTransfer(owner.address, nonOwner.address, 1, EMPTY_DATA, EMPTY_DATA)
+      assert.fail('Should not have succeeded!')
+    } catch (_err) {
+      const expectedErr = 'caller is not the admin operator'
+      assert(_err.message.includes(expectedErr))
+    }
   })
 
-  it('adminTransfer() should transfer tokens', async () => {
-    const tx = await pTokenContract.adminTransfer(OWNER, NON_OWNER, '12345', '0x', '0x', { from: ADMIN_OPERATOR })
-    await expectEvent(tx.receipt, 'Transfer', { value: '12345', from: OWNER, to: NON_OWNER })
-    expect(await pTokenContract.balanceOf(NON_OWNER)).to.be.bignumber.eq('12345')
+  it('`adminTransfer()` should transfer tokens', async () => {
+    const amount = '12345'
+    const contract = pTokenContract.connect(adminOperator)
+    const tx = await contract.adminTransfer(owner.address, nonOwner.address, amount, EMPTY_DATA, EMPTY_DATA)
+    const receipt = await tx.wait()
+    await assertTransferEvent(receipt, owner.address, nonOwner.address, amount)
+    const balance = await pTokenContract.balanceOf(nonOwner.address)
+    assert(balance.eq(BigNumber.from(amount)))
   })
 })
